265700d1b4fa962eceb91b7399b10bcb83081709
danilk3 <70892774+danilk3@users.noreply.github.com>
2022-10-11 13:36:27 +0300

    Данные для наполнения бд.

---
+++ sql_test_data/library/library.book_category.sql
@@ -0,0 +1,10 @@
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (1, null, 'Classics');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (2, null, 'Comic Book');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (3, null, 'Adventure');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (4, null, 'Fantasy');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (5, null, 'Historical Fiction');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (6, null, 'Horror');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (7, null, 'Detective');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (8, null, 'Romance');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (9, null, 'Sci-Fi');
+insert into library.book_category (book_category_id, parent_book_category_id, name) values (10, null, 'Thrillers');
